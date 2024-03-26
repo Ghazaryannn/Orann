486d0d62f9b2c861330b7b251fdb0272238cbb78
Kazaryannn <147075312+Kazaryannn@users.noreply.github.com>
2024-03-26 15:17:46 +0400

    orran

--- src/app/Menu/page.jsx
+++ src/app/Menu/page.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
-import Image from 'next/image'
 import styles from './page.module.css'
 
 const Menu = () => {
   return (
     <div className={styles.menu}>
-         <div className={styles.menu_header}>
+        <div className={styles.menu_header}>
             <p>Ուտեստներ</p>
-            <p>Հյութեր և Ըմպելիքներ</p>
-         </div>
-         <div className={styles.menu_center}>
-            <div className={styles.prod}>
-            </div>
-         </div>
+            <p>Ըմպելիքներ</p>
+        </div>
+        <div className={styles.appetizer}>
+            <h2>Նախաուտեստներ</h2>
+        </div>
+        <div className={styles.grilled}>
+            <h2>Թոնրի խորոված</h2>
+        </div>
     </div>
   )
 }
