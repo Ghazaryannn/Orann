478fbb026fbf9e2e2724276ff887d527d449f5d3
Kazaryannn <147075312+Kazaryannn@users.noreply.github.com>
2024-03-27 13:23:59 +0400

    orran

--- src/db.js
+++ src/db.js
@@ -4,7 +4,7 @@ export const snacks = [
         name: 'Հաց',
         image:'/hac.jpg',
         description: '',
-        price: '600 դ'
+        price: '600 Դ'
     },
     {
         id:2,
@@ -12,7 +12,7 @@ export const snacks = [
         image:'/panir_vochxari.jpg',
         description: '',
         size: '(200 գ)',
-        price: '1000 դ'
+        price: '1000 Դ'
     },
     {
         id:3,
@@ -20,27 +20,93 @@ export const snacks = [
         image:'/chanax.jpg',
         description: '',
         size: '(200 գ)',
-        price: '1000 դ'
+        price: '1000 Դ'
     },
     {
         id:4,
         name: 'Ռեժան',
         image:'/rejan.jpg',
         description: '',
-        price: '600 դ'
+        price: '600 Դ'
     },
     {
         id:5,
         name: 'Քամած մածուն ',
         image:'/macun.jpg',
         description: '',
-        price: '500 դ'
+        price: '500 Դ'
     },
     {
         id:6,
         name: 'Թթվի տեսականի',
         image:'/ttu.jpg',
         description: '',
-        price: '1200 դ'
+        price: '1200 Դ'
+    },
+    {
+        id:7,
+        name: 'Կանաչի',
+        image:'/kanachi.jpg',
+        description: '',
+        price: '800 Դ'
+    },
+    {
+        id:8,
+        name: 'Լիմոն',
+        image:'/limon.jpg',
+        description: '',
+        price: '300 Դ'
+    },
+    {
+        id:9,
+        name: 'Ձիթապտուղ',
+        image:'/zeytun.jpg',
+        description: '',
+        price: '800 Դ'
+    },
+    {
+        id:10,
+        name: 'Մոժոժ',
+        image:'/mojoj.jpg',
+        description: '',
+        price: '600 Դ'
+    },
+    {
+        id:11,
+        name: 'Ղաուրմա',
+        image:'/xaurma.jpg',
+        description: '',
+        size: '(200 գ)',
+        price: '1500 Դ'
+    },
+    {
+        id:12,
+        name: 'Բլինչիկ մսով',
+        image:'/blinchik.jpg',
+        description: '',
+        price: '400 Դ'
+    },
+    {
+        id:13,
+        name: 'Մսի տեսականի',
+        image:'/msi_tesakani.jpg',
+        description: '',
+        price: '4000 Դ'
+    },
+    {
+        id:14,
+        name: 'Տավարի լեզու',
+        image:'/lezu.jpg',
+        description: '',
+        size: '(120 գ)',
+        price: '500 Դ'
+    },
+    {
+        id:15,
+        name: 'Հումուս',
+        image:'/humus.jpg',
+        description: '',
+        size: '(100 գ)',
+        price: '500 Դ'
     },
 ]
